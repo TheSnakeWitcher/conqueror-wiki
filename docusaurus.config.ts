52d18496ca0e17111028459b1040cb7a4def787e
Mr.PAPI <thesnakewitcher@gmail.com>
2025-04-19 18:00:11 -0300

    add config files

---
+++ docusaurus.config.ts
@@ -0,0 +1,113 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+    title: 'Conqueror wiki',
+    tagline: 'Your portal to a healty life',
+    favicon: 'img/ConquerorLogo.ico',
+    url: 'https://your-docusaurus-site.example.com',
+    baseUrl: '/',
+    organizationName: 'conqueror',
+    projectName: 'conqueror',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en','es'],
+    },
+
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    routeBasePath: '/',
+                    sidebarPath: './sidebars.ts',
+                    editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                },
+                blog: false,
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        colorMode: {
+            disableSwitch: true,
+        },
+        navbar: {
+            title: 'Conqueror',
+            logo: {
+                alt: 'Conqueror Logo',
+                src: 'img/ConquerorLogo.png',
+            },
+            items: [
+                {
+                    href: 'https://instagram.com/conquerorlifestyle',
+                    label: 'Instagram',
+                    position: 'right',
+                },
+                {
+                    href: 'https://github.com/facebook/docusaurus',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Tutorial',
+                            to: '/docs/intro',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Stack Overflow',
+                            href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                        },
+                        {
+                            label: 'Discord',
+                            href: 'https://discordapp.com/invite/docusaurus',
+                        },
+                        {
+                            label: 'X',
+                            href: 'https://x.com/docusaurus',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'Blog',
+                            to: '/blog',
+                        },
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/facebook/docusaurus',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
+};
+
+export default config;
